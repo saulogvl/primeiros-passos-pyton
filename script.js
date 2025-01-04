c119f3b229349150afaeab8b293d39ab39e346ae
Saulo <saulogabrielvluna@gmail.com>
2025-01-04 13:49:12 +0000

    feat:<melhorei o design>

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-alert('Atenção calculos de porcentagem e alguns outros, ainda não foram adicionados!')
+let display = document.getElementById("display");
+
+let currentInput = "";
+
+function appendNumber(number) {
+    currentInput += number;
+
+    display.value = currentInput;
+}
+
+function appendOperator(operator) {
+
+    if (currentInput !== "") {
+
+        currentInput += operator;
+
+        display.value = currentInput;
+    }
+}
+
+function clearDisplay() {
+    currentInput = "";
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        
+        display.value = currentInput;
+    } catch (error) {
+        
+        display.value = "Erro";
+
+        currentInput = "";
+    }
+}
